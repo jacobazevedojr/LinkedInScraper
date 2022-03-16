501a32817bdfbed9ae4142270b63f1a8a52b800c
Jacob Azevedo Jr <39184485+jacobazevedojr@users.noreply.github.com>
2022-03-16 11:18:17 -0700

    Handles broken links and blocked scraping

--- LinkedInProfileExtraction.py
+++ LinkedInProfileExtraction.py
@@ -11,6 +11,10 @@ import time
 def scrapeProfiles():
     driver = LinkedInScraper(USERNAME, PASSWORD, DRIVER_PATH, DATABASE)
 
+    previousFail = ""
+    scrapingFailed = 0
+    successfulScrapes = 0
+    maxFailedScrapes = 5
     for i, URL in enumerate(driver.__employeeURLsToBeScraped__):
         if URL in driver.__employeeURLsInDB__:
             print(URL, "Is already a profile in the database")
@@ -19,15 +23,28 @@ def scrapeProfiles():
         try:
             empList = [driver.ExtractProfileAttributes(URL)]
 
+            # Scraping has failed in some way (but an exception is not thrown)
             if empList[0] is None:
-                # Write remaining employees to file
-                WriteLinesToFile("employeeURLs.txt", driver.__employeeURLsToBeScraped__[i:])
                 print("ERROR:", URL, "did not extract properly. There is either a bug or scraping was blocked.")
-                print(i, "profiles, successfully extracted")
-                break
+                # If a URL has failed twice, remove it from the list (doesn't account for blocked scraping,
+                # but losing one datapoint is ok)
+                if previousFail == URL:
+                    successfulScrapes = i + 1
+                    scrapingFailed += 1
+                    continue
+                previousFail == URL
+
+                # If scraping has failed 5 times in a row, stop scraping and eliminate from list
+                # This is to account for blocked scraping
+                # This makes an assumption that 5 broken URLs in a row is unlikely
+                if scrapingFailed > maxFailedScrapes - 1:
+                    successfulScrapes = i
+                    break
             else:
                 # Insert emp into database
+                scrapingFailed = 0
                 DATABASE.insertEmployees(empList)
+                successfulScrapes = i
         except:
             print(URL, "cannot be extracted, deleting")
             WriteLinesToFile("employeeURLs.txt", driver.__employeeURLsToBeScraped__[i+1:])
@@ -35,6 +52,9 @@ def scrapeProfiles():
                 file.write(URL + "\n")
             continue
 
+    # Rewrite file to chop off all successfully scraped files
+    WriteLinesToFile("employeeURLs.txt", driver.__employeeURLsToBeScraped__[successfulScrapes:])
+    print(successfulScrapes, "profiles, successfully extracted")
 
 
 schedule.every().day.at("00:00").do(scrapeProfiles)
